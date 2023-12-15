731a310bbe89a236192cf40f9c529c76b66b3611
Deeksha Tiwari <deekshatiwari@Deekshas-MacBook-Air.local>
2023-12-15 00:17:39 -0500

    resume scoring using openai api done

--- resume_score.py
+++ resume_score.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
+
 client = OpenAI()
 import docx
 
@@ -19,13 +20,25 @@ job_description = read_docx('jd.docx')
 
 # Combine resume and job description in a prompt for the API
 prompt = f"Resume: {resume}\n\nJob Description: {job_description}\n\n" \
-         "Score the resume on how well it matches the job description out of 100:"
-
-# Send the prompt to the OpenAI API
-response = client.completions.create(
-    engine="gpt-4",
-    prompt=prompt,
-    max_tokens=50)
+         "Score the resume on how well it matches the job description out of 100. Respond only with score out of 100"
 
 
-print(response.choices[0].text.strip())
+# # Send the prompt to the OpenAI API
+# response = client.chat.completions.create(
+#     model="gpt-4",
+#     prompt=prompt,
+#     max_tokens=50)
+
+response = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ]
+)
+
+# Extracting the message content from the response
+message_content = response.choices[0].message.content
+
+# Printing the message content
+print(message_content)
